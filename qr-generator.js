cc796b942ee370b2b4c76a34bd02512a55565b9b
Jyotirambedage <bedage12345@gmail.com>
2026-03-31 14:12:37 +0530

    Delete qr-generator.js

--- qr-generator.js
+++
@@ -1,9 +0,0 @@
-const textInput = document.getElementById('text-input');
-const generateBtn = document.getElementById('generate-btn');
-const qrPreview = document.getElementById('qr-preview');
-
-generateBtn.addEventListener('click', ()=>{
-    qrPreview.innerHTML='';
-    if(!textInput.value){ alert('Enter text or URL!'); return; }
-    new QRCode(qrPreview, { text:textInput.value, width:200, height:200 });
-});
